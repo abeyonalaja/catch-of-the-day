ec440ebabd8e7f4af698a6b262a2c6b06e0c8271
Abey Onalaja <abey.onalaja@gmail.com>
2018-05-17 22:26:24 +0100

    Show sold out for fish

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,21 +2,21 @@ import React from 'react'
 import { formatPrice } from '../helpers';
 
 class Fish extends React.Component {
-    render() { 
-        const { image, name, desc, status, price } = this.props.details;
-        
-        return ( 
-            <li className="menu-fish">
-                <img src={image} alt={name}/>
-                <h3 className="fish-name">
-                    {name}
-                    <span className="price">{formatPrice(price)}</span>
-                </h3>
-                <p>{desc}</p>
-                <button>Add To Cart</button>
-            </li>
-         );
-    }
+  render() {
+    const { image, name, desc, status, price } = this.props.details;
+    const isAvailable = status === 'available';
+    return (
+      <li className="menu-fish">
+        <img src={image} alt={name}/>
+        <h3 className="fish-name">
+          {name}
+          <span className="price">{formatPrice(price)}</span>
+        </h3>
+        <p>{desc}</p>
+        <button disabled={!isAvailable}>{isAvailable ? 'Add to Order' : 'Sold Out!'}</button>
+      </li>
+    );
+  }
 }
- 
+
 export default Fish;
