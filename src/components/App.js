50af2bf7887a08e612321b3bdef33cdb43e1f852
Abey Onalaja <abey.onalaja@gmail.com>
2018-05-17 22:41:57 +0100

    Update order status

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
   }
 
   addFish = fish => {
-      //take a copy of the existing state
+    //take a copy of the existing state
     const fishes = { ...this.state.fishes };
     // 2. Add our new fish to thAT FISHES VARIABLE
     fishes[`fish${Date.now()}`] = fish;
@@ -24,21 +24,29 @@ class App extends React.Component {
     this.setState({ fishes: sampleFishes });
   }
 
+  addToOrder = (key) => {
+    const order = {...this.state.order}
+    order[key] = order[key] + 1 || 1;
+    this.setState({order})
+  }
+
   render(){
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Abey is the best"/>
           <ul className="fishes" >
-            {Object.keys(this.state.fishes).map(key => 
-              <Fish key={key} 
-              details={this.state.fishes[key]} />)}
+            {Object.keys(this.state.fishes).map(key =>
+              <Fish key={key}
+                    index={key}
+                    details={this.state.fishes[key]}
+                    addToOrder={this.addToOrder}/>)}
           </ul>
         </div>
         <Order />
         <Inventory
-          addFish={this.addFish}
-          loadSampleFishes={this.loadSampleFishes} />
+            addFish={this.addFish}
+            loadSampleFishes={this.loadSampleFishes} />
       </div>
     );
   }
