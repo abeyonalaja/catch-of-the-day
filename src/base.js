627b49947f5a04002a363cac3e8276252263009d
Abey Onalaja <abey.onalaja@gmail.com>
2018-06-11 22:08:36 +0100

    removed api key

--- src/base.js
+++ src/base.js
@@ -1,12 +1,11 @@
 import Rebase from 're-base';
 import firebase from 'firebase';
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDR_oiUHj70PcVAzQ4DB7yCF9TDbU0oqhs",
     authDomain: "catch-of-the-day-abey.firebaseapp.com",
     databaseURL: "https://catch-of-the-day-abey.firebaseio.com"
 });
 
-const bae = Rebase.createClass(firebaseApp.database());
+const base = Rebase.createClass(firebaseApp.database());
 
 export { firebaseApp };
 
